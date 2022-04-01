2bdead5a81323bc98365b14f8c3532974547aff6
Justin Sixsmith <justinsixsmith@gmail.com>
2022-04-01 09:03:47 -1000

    adds unique index to albums

---
+++ indexes.sql
@@ -0,0 +1,26 @@
+use justin;
+
+describe albums;
+
+ALTER TABLE albums
+    ADD CONSTRAINT name_artist UNIQUE (name, artist);
+
+describe board_games;
+
+describe departments;
+
+describe departments;
+
+describe dept_manager;
+
+describe employees;
+
+describe my_kids;
+
+describe quotes;
+
+describe quotes;
+
+describe salaries;
+
+describe titles;
