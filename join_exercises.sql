0978b1f5c8e5178f72d099553d0256e853d27bc7
Justin Sixsmith <justinsixsmith@gmail.com>
2022-04-11 10:49:29 -0500

    adds bonus for joins_exercises

--- join_exercises.sql
+++ join_exercises.sql
@@ -7,7 +7,7 @@ from departments
               on departments.dept_no = dept_manager.dept_no
          join employees
               on dept_manager.emp_no = employees.emp_no
-where to_date = '9999-01-01';
+where to_date > curdate();
 
 
 select departments.dept_name, concat(first_name, ' ', last_name) as department_manager
@@ -16,7 +16,7 @@ from departments
               on departments.dept_no = dept_manager.dept_no
          join employees
               on dept_manager.emp_no = employees.emp_no
-where to_date = '9999-01-01'
+where to_date > curdate()
   and employees.gender like 'f';
 
 
@@ -28,8 +28,8 @@ from employees
               on dept_emp.dept_no = departments.dept_no
          join titles
               on employees.emp_no = titles.emp_no
-where dept_emp.to_date = '9999-01-01'
-  and titles.to_date = '9999-01-01'
+where dept_emp.to_date > curdate()
+  and titles.to_date > curdate()
   and dept_name like 'customer service'
 group by title
 order by total desc;
@@ -43,21 +43,18 @@ from departments
               on employees.emp_no = dept_manager.emp_no
          join salaries
               on employees.emp_no = salaries.emp_no
-where dept_manager.to_date = '9999-01-01'
-  and salaries.to_date = '9999-01-01';
+where dept_manager.to_date > curdate()
+  and salaries.to_date > curdate();
 
 
-select concat(first_name, ' ', last_name) as employee,
-       dept_name,
-       concat(first_name, ' ', last_name) as department_manager
-from employees
-         join dept_emp
-              on employees.emp_no = dept_emp.emp_no
-         join departments
-              on dept_emp.dept_no = departments.dept_no
-         join dept_manager
-              on departments.dept_no = dept_manager.dept_no
-where dept_emp.to_date = '9999-01-01'
-  and dept_manager.to_date = '9999-01-01'
-union select concat(first_name, ' ', last_name) as department_manager
-from employees;
+select concat(e.first_name, ' ', e.last_name)   as Employee,
+       d.dept_name                              as 'Department Name',
+       concat(e2.first_name, ' ', e2.last_name) as 'Department Manager'
+from employees e
+         join dept_emp de on e.emp_no = de.emp_no
+         join departments d on de.dept_no = d.dept_no
+         join dept_manager dm on d.dept_no = dm.dept_no
+         join employees e2 on e2.emp_no = dm.emp_no
+where de.to_date > curdate()
+and dm.to_date > curdate();
+
