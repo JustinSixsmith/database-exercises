b25d0088b9806d0b653b8c2a1e630c4012b8932f
Justin Sixsmith <justinsixsmith@gmail.com>
2022-04-11 12:52:23 -0500

    adds subqueries exercises through bonus 1

---
+++ subqueries_exercises.sql
@@ -0,0 +1,43 @@
+use justin;
+
+
+select concat(first_name, ' ', last_name) as Employee
+from employees
+where hire_date in (
+    select hire_date
+    from employees
+    where emp_no = '101010');
+
+
+select title, count(title)
+from titles
+where emp_no in (
+    select employees.emp_no
+    from employees
+    where first_name = 'Aamod'
+)
+group by title;
+
+
+select first_name, last_name
+from employees
+where gender = 'f'
+  and emp_no in (
+    select emp_no
+    from dept_manager
+    where to_date > curdate()
+);
+
+
+select dept_name
+from departments
+where dept_no in (
+    select dept_no
+    from dept_manager
+    where to_date > curdate()
+      and emp_no in (
+        select emp_no
+        from employees
+        where gender = 'f'
+    )
+);
